8fad6eab7d92dda5a06be2db0c8136733ef7e9dc
Yohan, Anushka <Anushka.Yohan@Pearson.com>
2018-07-31 18:28:51 +0530

    changed the greeting method to include the user name

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,8 +1,11 @@
-package com.example.demo.controller;
+package com.example.demo.controller; 
+
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
@@ -10,7 +13,20 @@ public class MainController {
 	
 	@RequestMapping("greet")
 	public ResponseEntity<String> greet() {
-		return new ResponseEntity<>("Welcome!", HttpStatus.OK);
+		return new ResponseEntity<>("Welcome to Spring Boot on Jenkins", HttpStatus.OK);
 	}
 
+
+	@RequestMapping("user")
+	public ResponseEntity<String> greetSpecificUser(@RequestParam Map<String, String> reqParams) {
+		
+		String user = "user";
+		
+		// greet user by his/her name if name is present in url, else opt to default greeting.
+		if (reqParams.containsKey("user")) {
+			user = reqParams.get("user");
+		}
+		
+		return new ResponseEntity<>("Hello, " + user + "!", HttpStatus.OK);
+    }
 }
